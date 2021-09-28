95cf1c09e1a74369e5c21af95081e63b67f993c1
DmitriiFadeev <dmitriifadeev0702@gmail.com>
2021-09-28 22:18:54 +0500

    addTask

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,54 @@
 <template>
   <div class="wrapper">
     <div class="header">
-        <div class="name">NAME</div>
-        <div class="board">Kanban board</div>
-        <div class="filter">
-            <span>quick filtres:</span>
+        <div>
+            <span>Добавить задачу</span>
+            <div>
+                <span>Выберете раздел</span>
+                <select name="" id="r1">
+                    <option 
+                    v-for="category in categoties" 
+                    :key="category.id" 
+                    v-bind:value="category.id"
+                    >
+                    {{category.title}}
+                    </option>
+                </select>
+            </div>
+            <div>
+                <span>Выберете наблюдателя</span>
+                <select name="" id="n1">
+                    <option
+                    v-for="item in items"
+                    :key="item.id" 
+                    v-bind:value="item.user">
+                    {{item.user}}
+                    </option>
+                </select>
+            </div>
+            <div>
+                <span>Введите название задачи</span>
+                <input id="t1" type="text">
+            </div>
+            <div>
+                <span>Введите статус задачи</span>
+                <input id="s1" type="text">
+            </div>
+            <div>
+                <span>Введите исполнителей</span>
+                <div class="performers">
+                    <div 
+                        v-for="performer in performers"
+                        :key="performer.id"
+                        class="performer"
+                        v-bind:id="'performer_'+performer.id"
+                    >
+                        <input type="checkbox" v-bind:id="performer.id">
+                        <span>{{performer.name}}</span>
+                    </div>
+                </div>
+            </div>
+            <input @click="addTask" id="btn" type="button" value="добавить задачу">
         </div>
     </div>
     <div class="content">
@@ -16,7 +60,6 @@
         @dragenter.prevent
         class="content-item">
             <div class="content-item__title">
-                <span class="count">3</span>
                 <div>{{category.title}}</div>
             </div>
             <div class="content-item__list">
@@ -27,7 +70,6 @@
                 draggable="true"
                 class="item">
                     <div class="task">
-                        <div class="performer"></div>
                         <div class="task-name">
                             <div>{{item.user}}</div>
                             <div>{{item.title}}</div>
@@ -48,19 +90,18 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-
 export default {
 
   name: 'App',
-  setup() {
-      const items = ref([
+  data(){
+    return {
+        items: [
           {
               id: 6,
               user: 'admin1',
               title: 'Бронирование лота в предверии торгов',
               status: 'Ожидание работ',
-              performer: [{ id:1, name: 'Антон'},{id:2, name: 'Георгий'}],
+              performer: [{ id:1, name: 'Антон'}],
               categoryId: 0,
           },
           {
@@ -68,7 +109,7 @@ export default {
               user: 'admin2',
               title: 'Дата провередения торгов для комиссиональной торговли',
               status: 'Ожидание работ',
-              performer: [{ id:17, name: 'Борис'},{id:27, name: 'Георгий'}],
+              performer: [{ id:17, name: 'Борис'}],
               categoryId: 0,
           },
           {
@@ -76,7 +117,7 @@ export default {
               user: 'admin3',
               title: 'Рефакторинг входа в админку',
               status: 'В работе',
-              performer: [{ id:15, name: 'Антон'},{id:24, name: 'Алексей'}],
+              performer: [{ id:15, name: 'Антон'}],
               categoryId: 1,
           },
           {
@@ -84,7 +125,7 @@ export default {
               user: 'admin4',
               title: 'Правки в квитанции по оплате',
               status: 'В работе',
-              performer: [{ id:13, name: 'Антон'},{id:24, name: 'Максим'}],
+              performer: [{ id:13, name: 'Антон'}],
               categoryId: 1,
           },
           {
@@ -92,15 +133,15 @@ export default {
               user: 'admin5',
               title: 'API. Начало',
               status: 'Ожидание тестирования',
-              performer: [{ id:11, name: 'Роман'},{id:23, name: 'Георгий'}],
+              performer: [{ id:11, name: 'Роман'}],
               categoryId: 2,
           },
           {
               id: 26,
               user: 'admin6',
               title: 'Раздел "продавцы" на yii',
-              status: 'Ожидание тесстирования',
-              performer: [{ id:13, name: 'Вячеслав'},{id:22, name: 'Илья'}],
+              status: 'Ожидание тестирования',
+              performer: [{ id:13, name: 'Вячеслав'}],
               categoryId: 2,
           },
           {
@@ -108,7 +149,7 @@ export default {
               user: 'admin7',
               title: 'Ошибка формирования протокола',
               status: 'Парное тестирование',
-              performer: [{ id:1, name: 'Александр'},{id:2, name: 'Инна'}],
+              performer: [{ id:1, name: 'Александр'}],
               categoryId: 3,
           },
           {
@@ -116,7 +157,7 @@ export default {
               user: 'admin8',
               title: 'Мои данные, Сокращенное',
               status: 'Тестирование',
-              performer: [{ id:3, name: 'Александр'},{id:4, name: 'Юлия'}],
+              performer: [{ id:3, name: 'Александр'}],
               categoryId: 3,
           },
           {
@@ -124,7 +165,7 @@ export default {
               user: 'admin9',
               title: 'Ошибки в дате окончания подачи заявки',
               status: 'Ревизия функциональности',
-              performer: [{ id:1, name: 'Антон'},{id:2, name: 'Георгий'}],
+              performer: [{ id:1, name: 'Антон'}],
               categoryId: 4,
           },
           {
@@ -132,12 +173,11 @@ export default {
               user: 'admin0',
               title: 'Ошибка 404',
               status: 'Ревизия функциональности',
-              performer: [{ id:3, name: 'Павел'},{id:4, name: 'Алексей'}],
+              performer: [{ id:3, name: 'Павел'}],
               categoryId: 4,
           }
-      ])
-
-      const categoties = ref([
+      ],
+      categoties: [
           {
               id: 0,
               title: 'К работе'
@@ -158,29 +198,74 @@ export default {
               id: 4,
               title: 'Почти готово'
           }
-      ])
-
-      const onDragStart = (e, item) => {
+      ],
+      performers: [
+          {
+              id:1, name: 'Антон'
+          },
+          {   
+              id:2, name: 'Георгий'
+          },
+          {   
+              id:3, name: 'Алексей'
+          },
+          {   
+              id:4, name: 'Георгий'
+          },
+          {   
+              id:5, name: 'Константин'
+          },
+          {   
+              id:6, name: 'Павел'
+          },
+      ]
+    }
+  },
+  methods: {
+      onDragStart(e, item){
           e.dataTransfer.dropEffect = 'move'
           e.dataTransfer.effectAllowed = 'move'
           e.dataTransfer.setData('itemId',item.id.toString())
-      }
+      },
 
-      const onDrop = (e, categoryId) => {
+      onDrop(e, categoryId){
           const itemId = parseInt(e.dataTransfer.getData('itemId'))
-          items.value = items.value.map(x => {
-              if(x.id == itemId)
-                  x.categoryId = categoryId
+          this.items = this.items.map(x => {
+              if(x.id == itemId){
+                x.categoryId = categoryId
+              }
             return x
           })
-          console.log('items', items)
-      }
+      },
 
-      return {
-          items, 
-          categoties,
-          onDragStart,
-          onDrop
+      addTask(){
+        const category = document.querySelector('#r1').value
+        const user = document.querySelector('#n1').value
+        const title = document.querySelector('#t1').value
+        const status = document.querySelector('#s1').value
+        const performers = document.querySelector('.performers')
+
+        let isp = performers.querySelectorAll('input');
+
+        let s = [];
+        
+        isp.forEach(element => {
+            if(element.checked){
+                let per = document.querySelector(`#performer_${element.id}`)
+                let id = per.querySelector('input').id
+                let name = per.querySelector('span').innerHTML
+                s.push({id, name})
+            }
+        });
+        
+        this.items.push({
+            id: Math.floor(Math.random() * 3),
+            user: user,
+            title: title,
+            status: status,
+            performer: s,
+            categoryId: category,
+        })
       }
   },
 }
@@ -211,6 +296,12 @@ body{
 .header .filter{
     font-size: 14px;
     color: black;
+}
+.header>div>div{
+    margin: 5px 0;
+}
+.header>div>div>span{
+    margin-right: 10px;
 }
 .content{
     border-top: 1px solid #979797;
@@ -264,12 +355,12 @@ body{
 .content .content-item .content-item__list .item .task .avatar{
     width: 20px;
     height: 20px;
-    background-color: red;
+    background-color: pink;
 }
 .content .content-item .content-item__list .item .status{
     display: flex;
     flex-direction: column;
-    padding-left: 10%;
+    padding: 10px;
 }
 .content .content-item .content-item__list .item .status div{
     font-size: 14px;
